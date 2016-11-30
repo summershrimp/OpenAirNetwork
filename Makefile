d6a33fb67b735b7f5c54d75e02d55fcf2c5884a1
Yibai Zhang <xm1994@gmail.com>
2016-11-30 23:02:28 +0800

    Add Makefile, arrange headers. Try to add yaml config but not finished yet.

---
+++ Makefile
@@ -0,0 +1,29 @@
+DIR_INC = ./include
+DIR_SRC = ./src
+DIR_OBJ = ./obj
+DIR_BIN = ./bin
+
+SRC = $(wildcard ${DIR_SRC}/*.cpp)  
+OBJ = $(patsubst %.cpp,${DIR_OBJ}/%.o,$(notdir ${SRC})) 
+
+TARGET = an
+
+BIN_TARGET = ${DIR_BIN}/${TARGET}
+
+CC = g++
+CFLAGS = -O2 -Wall -I${DIR_INC}
+LDFLAGS = -lyaml
+
+${BIN_TARGET}:${OBJ}
+	    $(CC) $(OBJ)  -o $@ $(LDFLAGS)
+		    
+${DIR_OBJ}/%.o:${DIR_SRC}/%.cpp prereq
+	    $(CC) $(CFLAGS) -c  $< -o $@
+
+prereq: 
+	mkdir -p obj
+	mkdir -p bin
+
+.PHONY:clean
+clean:
+	find ${DIR_OBJ} -name *.o -exec rm -rf {}
--- src/config.cpp
+++ src/config.cpp
@@ -1,6 +1,8 @@
 #include "config.h"
 #include <string.h>
 #include <arpa/inet.h>
+#include <yaml.h>
+
 craft_addr crafts[10];
 proto_type protos[128];
 
@@ -11,7 +13,44 @@ int is_master = 1;
 int craft_cnt = 1;
 int event_once = 100;
 
+yaml_parser_t parser;
+yaml_event_t event;
+yaml_document_t document;
 int an_load_config() {
+    
+    yaml_parser_initialize(&parser);
+    FILE *fp = fopen("config.yml", "r");
+    yaml_parser_set_input_file(&parser, fp);
+    int done = 0;
+    yaml_event_type_t last_event = YAML_NO_EVENT;
+    while(!done) {
+        if(!yaml_parser_parse(&parser, &event)) {
+            printf("Parse yaml config file failed.");
+            exit(0);
+        }
+        switch(event.type) {
+        	case YAML_NO_EVENT: puts("No event!"); break;
+	       	case YAML_STREAM_START_EVENT: puts("STREAM START"); break;
+    		case YAML_STREAM_END_EVENT:   puts("STREAM END");   break;
+    		case YAML_DOCUMENT_START_EVENT: puts("<b>Start Document</b>"); break;
+    		case YAML_DOCUMENT_END_EVENT:   puts("<b>End Document</b>");   break;
+    		case YAML_SEQUENCE_START_EVENT: puts("<b>Start Sequence</b>"); break;
+    		case YAML_SEQUENCE_END_EVENT:   puts("<b>End Sequence</b>");   break;
+    		case YAML_MAPPING_START_EVENT:  puts("<b>Start Mapping</b>");  break;
+    		case YAML_MAPPING_END_EVENT:    puts("<b>End Mapping</b>");    break;
+    		case YAML_ALIAS_EVENT:  printf("Got alias (anchor %s)\n", event.data.alias.anchor); break;
+    		case YAML_SCALAR_EVENT: printf("Got scalar (value %s)\n", event.data.scalar.value); break;
+ 
+        }
+        last_event = event.type;
+        done = (event.type == YAML_STREAM_END_EVENT);
+        yaml_event_delete(&event);
+        
+    }
+    yaml_parser_delete(&parser);
+    fclose(fp);
+
+
     crafts[0].addr = inet_addr("192.168.59.3");
     crafts[0].send_seq = 0;
     crafts[0].recv_seq = 0;
