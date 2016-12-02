42be038c5e7780c322d1fe7868659f057d3047cc
Yibai Zhang <xm1994@gmail.com>
2016-12-02 12:00:50 +0800

    Add onboard application example.

--- test_utils/app_emu.cpp
+++ test_utils/app_emu.cpp
@@ -3,17 +3,20 @@
 #include <fcntl.h>
 #include <string.h>
 int main(void) {
-    int fd = open("/tmp/fifo1in", O_RDONLY),size;
+    int fd_in = open("/tmp/fifo1in", O_RDONLY),size;
+    int fd_out = open("/tmp/fifo1out", O_WRONLY);
     char buf[1450];
-    if(fd == -1) {
+    if(fd_in == -1) {
         perror("fd");
         return 0;
     }
+    write(fd_out,"Test boardcast", 14);
     while(true) {
-        size = read(fd, buf, sizeof(buf));
+        size = read(fd_in, buf, sizeof(buf));
         if(size == 0) {
             puts("size = 0, wait...");
             sleep(1);
+            continue;
         }
         buf[size] = 0;
         printf("size:%d\n", size);
